51af03dfd973396d27cc4328cf24bd77f7d43d72
hyeva002 <412810549@qq.com>
2016-12-10 00:40:47 +0800

    The first committed master branch of guest

---
+++ sign/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from sign.models import Event, Guest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404
+
+def index(request):
+    return render(request, 'index.html')
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/event_manage/')
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    username = request.session.get('user', '')
+    return render(request, "event_manage.html", {"user":username, "events":event_list})
+
+@login_required
+def sreach_name(request):
+    username = request.session.get('user', '')
+    sreach_name = request.GET.get("name", "")
+    event_list = Event.objects.filter(name__contains = sreach_name)
+    return render(request, "event_manage.html", {"user":username, "events":event_list})
+
+@login_required
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+    # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+    # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+@login_required
+def sreach_guest(request):
+    username = request.session.get('user', '')
+    sreach_guest = request.GET.get("realname", "")
+    guest_list = Guest.objects.filter(realname__contains = sreach_guest)
+    return render(request, "guest_manage.html", {"user":username, "guests":guest_list})
+
+@login_required
+def sreach_phone(request):
+    search_phone = request.GET.get("phone","")
+    #print(search_name)
+    data = Guest.objects.filter(phone__contains=search_phone)
+    username = request.session.get('userinfo', '')
+    return render(request,"guest_manage.html",{"username":username,"guests":data})
+
+@login_required
+def sign_index(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, 'sign_index.html', {'event': event})
+
+# 签到动作
+@login_required
+def sign_index_action(request,event_id):
+    event = get_object_or_404(Event, id=event_id)
+    phone = request.POST.get('phone', '')
+
+    result = Guest.objects.filter(phone = phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'phone error.'})
+
+    result = Guest.objects.filter(phone = phone, event_id = event_id)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'event id or phone error.'})
+
+    result = Guest.objects.get(phone = phone)
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event, 'hint': "user has sign in."})
+
+    else:
+        Guest.objects.filter(phone = phone).update(sign = '1')
+        return render(request, 'sign_index.html', {'event': event, 'hint':'sign in success!',
+        'guest': result})
+
+# 退出登录
+@login_required
+def logout(request):
+    auth.logout(request) #退出登录
+    response = HttpResponseRedirect('/')
+    return response
